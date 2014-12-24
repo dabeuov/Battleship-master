1f49e188d3cb3af4e18d5cde599da8f8755d10c6
Danik <dan2907@mail.ru>
2014-12-24 23:51:14 +0600

    24 december

---
+++ src/BattleshipControllerInterface.java
@@ -0,0 +1,11 @@
+/**
+ * Controller for Battleship game. Contains one static method, to handle the end of the
+ * game. 
+ * 
+ * @author Nathan Newman
+ * 
+ */
+public interface BattleshipControllerInterface {
+
+
+}
